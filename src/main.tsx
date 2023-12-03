1c40e621bc468b3de2070633e05cb4e035d18b84
abdurrozaqf <rozaqa27@gmail.com>
2023-12-03 23:21:11 +0700

    feat: add dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import ReactDOM from "react-dom/client";
 import App from "./routers";
 import "./styles/index.css";
+import { ThemeProvider } from "./utils/contexts/theme-provider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </>
 );
