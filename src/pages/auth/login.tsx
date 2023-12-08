c32407223b5094a1d5c54f1d8289fd87febb721a
abdurrozaqf <rozaqa27@gmail.com>
2023-12-08 15:13:51 +0700

    fix: layout auth page

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -3,18 +3,21 @@ import { Toaster } from "@/components/ui/toaster";
 
 const Login = () => {
   return (
-    <div className="w-full h-screen flex items-center font-inter overflow-auto">
-      <div className="container w-full md:w-[34.75rem] h-full md:h-fit p-8 md:p-14 lg:py-20 lg:px-14 flex flex-col items-center justify-center bg-white dark:bg-black/25 dark:border rounded-md border shadow">
-        <h1 className="text-4xl font-black mb-16">
-          Cloud<span className="text-teal-600">bite</span>
-        </h1>
-        <p className="font-inter text-center text-3xl font-semibold mb-2">
-          Login
-        </p>
-        <p className="text-[#ABABAB] dark:text-white text-base font-normal mb-10">
-          Sign in to continue
-        </p>
-        <LoginForm />
+    <div className="w-full h-screen flex flex-col font-inter transition-all overflow-auto">
+      <div className="grow overflow-auto flex flex-col items-center justify-center p-6">
+        <div className="container w-full md:w-[34.75rem] h-full md:h-fit p-8 md:p-14 lg:py-20 lg:px-14 flex flex-col items-center justify-center bg-white dark:bg-black/25 dark:border rounded-md border shadow">
+          <h1 className="text-5xl tracking-tight mb-16">
+            <span className="font-bold">Cloud</span>
+            <span className="text-[#6224C3] font-black">bite</span>
+          </h1>
+          <p className="font-inter text-center text-3xl font-semibold mb-2">
+            Login
+          </p>
+          <p className="text-[#ABABAB] dark:text-white text-base font-normal mb-10">
+            Sign in to continue
+          </p>
+          <LoginForm />
+        </div>
       </div>
       <Toaster />
     </div>
